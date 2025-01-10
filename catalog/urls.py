82f11361baf76868a6b2dda7de2cea12f54571db
mihail <mihail.bashkatov@gmail.com>
2025-01-10 05:24:24 +0100

    Addedd paths for DetailView and ListView

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import ProductsListView, ProductDetailView
 
 app_name = 'catalog'
 
@@ -7,7 +8,8 @@ app_name = 'catalog'
 urlpatterns = [
       path('home/', views.home, name='home'),
       path('contacts/', views.contacts, name='contacts'),
-      path('index/<product_id>/', views.index, name='index'),
-      path('products_list/', views.products_list, name='products_list'),
-
+      # path('index/<product_id>/', views.index, name='index'),
+      # path('products_list/', views.products_list, name='products_list'),
+      path('products_list/', ProductsListView.as_view() , name='products_list'),
+      path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
