89356378cbeef39b5311057452deed91af67b0fd
mihail <mihail.bashkatov@gmail.com>
2025-01-12 16:02:04 +0100

    Configuration is added for Blog and Author in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Blog, Author
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'author', 'picture',)
+    list_filter = ('title', 'author',)
+    search_fields = ('title', 'content', 'author')
+
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('author_name',)
+    list_filter = ('author_name',)
+    search_fields = ('author_name',)
