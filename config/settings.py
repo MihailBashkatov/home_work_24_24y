efad89ba948927b4883afc0262dc07168c112fb7
mihail <mihail.bashkatov@gmail.com>
2025-01-12 16:04:50 +0100

    Minor adjustments in config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIR = [BASE_DIR/ 'static']
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
