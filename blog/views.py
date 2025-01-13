d05493dbd2612cf3101c391dbb8990e817a95c0e
mihail <mihail.bashkatov@gmail.com>
2025-01-13 17:20:42 +0100

    Created Classes BlogListView and BlogDetailView

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, ListView, DetailView, TemplateView, View
+from django.contrib import messages
+
+from catalog.models import Product
+from .models import Blog
+
+#
+# # The class for rendering the Home page
+# class HomeTemplateView(TemplateView):
+#     template_name = 'catalog/home.html'
+
+# The class for page with all blogs
+class BlogListView(ListView):
+    model = Blog
+
+
+# The class for viewing page for particular Blog
+class BlogDetailView(DetailView):
+    model = Blog
+#
+# # The class for creating Contacts page
+# class ContactsTemplateView(View):
+#     template_name = 'catalog/contacts.html'
+#
+#     def get(self, request):
+#         """ Returns html file with contacts page and contacts list"""
+#         contacts_company = Contact.objects.all()
+#         return render(self.request, 'catalog/contacts.html', {'contacts_company': contacts_company})
+#
+#
+#     def post(self, request):
+#         """ Returns html file with contacts page and confirm that message was sent successfully """
+#         name = request.POST.get('name')
+#         messages.success(self.request, f'Thanks, {name}, message submitted successfully.')
+#         return redirect('home:contacts_template')
