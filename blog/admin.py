1638e572d950cf5cc788467ba4db4554ecdfeb90
mihail <mihail.bashkatov@gmail.com>
2025-01-13 18:21:54 +0100

    Deleteing adminpanel for author

--- blog/admin.py
+++ blog/admin.py
@@ -1,18 +1,10 @@
 from django.contrib import admin
 
-from blog.models import Blog, Author
+from blog.models import Blog
 
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'author', 'picture',)
-    list_filter = ('title', 'author',)
-    search_fields = ('title', 'content', 'author')
-
-
-
-@admin.register(Author)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('author_name',)
-    list_filter = ('author_name',)
-    search_fields = ('author_name',)
+    list_display = ('title', 'content', 'picture',)
+    list_filter = ('title',)
+    search_fields = ('title', 'content',)
