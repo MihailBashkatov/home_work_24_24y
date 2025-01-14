6bb4b33773d26af196de7355c0dcea4522eb34fa
mihail <mihail.bashkatov@gmail.com>
2025-01-14 16:58:46 +0100

    Adding path blog_update

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
 from blog.apps import BlogConfig
 
 app_name = BlogConfig.name
@@ -9,4 +9,6 @@ urlpatterns = [
       path('blog_list/', BlogListView.as_view() , name='blog_list'),
       path('blog_detail/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
       path('blog_create', BlogCreateView.as_view(), name='blog_create'),
+      path('blog_update', BlogUpdateView.as_view(), name='blog_update'),
+
 ]
