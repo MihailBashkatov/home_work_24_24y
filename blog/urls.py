eaebea2463cc8a3316da224d5088fa5dd7e42eed
mihail <mihail.bashkatov@gmail.com>
2025-01-14 17:46:54 +0100

    Adding path blog_delete

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 from blog.apps import BlogConfig
 
 app_name = BlogConfig.name
@@ -9,6 +9,7 @@ urlpatterns = [
       path('blog_list/', BlogListView.as_view() , name='blog_list'),
       path('blog_detail/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
       path('blog_create', BlogCreateView.as_view(), name='blog_create'),
-      path('blog_update', BlogUpdateView.as_view(), name='blog_update'),
+      path('blog_update/<int:pk>/', BlogUpdateView.as_view(), name='blog_update'),
+      path('blog_delete/<int:pk>/', BlogDeleteView.as_view(), name='blog_delete'),
 
 ]
