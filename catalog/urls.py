1c5c5c57fe24ca82ac5b5d93d06867bd0c4fa70a
mihail <mihail.bashkatov@gmail.com>
2025-01-29 11:41:37 +0100

    Adding path ProductCreateView in urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import ProductsListView, ProductDetailView, ContactsTemplateView, HomeTemplateView
+from .views import ProductsListView, ProductDetailView, ContactsTemplateView, HomeTemplateView, ProductsCreateView
 
 app_name = 'catalog'
 
 """ Registering URL adresses for catalog app"""
 urlpatterns = [
       path('home/', HomeTemplateView.as_view(), name='home_template'),
+      path('products/create/', ProductsCreateView.as_view() , name='products_create'),
       path('products_list/', ProductsListView.as_view() , name='products_list'),
       path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
       path('contacts/', ContactsTemplateView.as_view(), name='contacts_template'),
