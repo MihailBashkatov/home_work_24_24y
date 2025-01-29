e9b2f7e22202fd5ebf11df3da139ff7a07657310
mihail <mihail.bashkatov@gmail.com>
2025-01-29 12:41:49 +0100

    Adding path ProductUpdateView in urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import ProductsListView, ProductDetailView, ContactsTemplateView, HomeTemplateView, ProductsCreateView
+from .views import ProductsListView, ProductDetailView, ContactsTemplateView, HomeTemplateView, ProductsCreateView, \
+      ProductsUpdateView
 
 app_name = 'catalog'
 
@@ -7,6 +8,7 @@ app_name = 'catalog'
 urlpatterns = [
       path('home/', HomeTemplateView.as_view(), name='home_template'),
       path('products/create/', ProductsCreateView.as_view() , name='products_create'),
+      path('products/update/<int:pk>/', ProductsUpdateView.as_view(), name='products_update'),
       path('products_list/', ProductsListView.as_view() , name='products_list'),
       path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
       path('contacts/', ContactsTemplateView.as_view(), name='contacts_template'),
