66f255c2897c1de5e278c1f71b6567541a3ed3eb
mihail <mihail.bashkatov@gmail.com>
2025-01-29 15:58:45 +0100

    Adding validator against prfohibited words in product name and description in forms.py

--- catalog/forms.py
+++ catalog/forms.py
@@ -3,11 +3,25 @@ from django.core.exceptions import ValidationError
 
 from .models import Product
 
+""" List of prohibited words"""
+PROHIBITED_WORDS = ['bes', 'tol']
+
 #Creating class ModelForm for adding product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['product_name', 'product_description', 'product_image', 'category', 'product_price']
+
+    #Validator against word in PROHIBITED WORDS list in the name of the product or in the description
+    def clean(self):
+        cleaned_data = super().clean()
+        product_name = cleaned_data.get('product_name')
+        product_description = cleaned_data.get('product_description')
+        for word in PROHIBITED_WORDS:
+            if word in product_name or word in product_description:
+                raise ValidationError(f"Word {word} is not possible to use nor in Product name neither in description")
+
+        return cleaned_data
 
 #     def __init__(self, *args, **kwargs):
 #         super(AuthorForm, self).__init__(*args, **kwargs)
