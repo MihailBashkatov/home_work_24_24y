375f849b0aa811cb18acb8a7d6906e6d6e81faf6
mihail <mihail.bashkatov@gmail.com>
2025-01-29 13:09:12 +0100

    Adding path ProductDeleteView in urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import ProductsListView, ProductDetailView, ContactsTemplateView, HomeTemplateView, ProductsCreateView, \
-      ProductsUpdateView
+      ProductsUpdateView, ProductDeleteView
 
 app_name = 'catalog'
 
@@ -10,6 +10,7 @@ urlpatterns = [
       path('products/create/', ProductsCreateView.as_view() , name='products_create'),
       path('products/update/<int:pk>/', ProductsUpdateView.as_view(), name='products_update'),
       path('products_list/', ProductsListView.as_view() , name='products_list'),
+      path('products/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
       path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
       path('contacts/', ContactsTemplateView.as_view(), name='contacts_template'),
 ]
