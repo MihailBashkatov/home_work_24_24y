536f8e75ed26a6810e9b86980ee6fa8f96f0da08
mihail <mihail.bashkatov@gmail.com>
2025-02-07 16:29:24 +0100

    Adding urls for login, logout and register in urls.py

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+from users.views import RegisterView
+
+app_name = 'users'
+
+""" Registering URL adresses for users app"""
+urlpatterns = [
+      path('login/', LoginView.as_view(), name='login'),
+      path('logout/', LogoutView.as_view(), name='logout'),
+      path('register/', RegisterView.as_view(), name='register'),
+]
