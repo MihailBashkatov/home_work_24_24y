549887d60ba8de2c90da0233e7e92190e5841a4d
mihail <mihail.bashkatov@gmail.com>
2025-02-16 13:17:07 +0100

    Adding decorator for cashe in ProductDetailView in catalog/views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 from django.urls import reverse_lazy
 from django.views.generic import (
     CreateView,
@@ -49,7 +51,8 @@ class ProductsListView(ListView):
         return queryset.filter(product_is_published=True)
 
 
-# The class for viewing page for particular product
+# The class for viewing page for particular product. With decorator fo cashes
+@method_decorator(cache_page(60 * 15), name='dispatch')
 class ProductDetailView(LoginRequiredMixin, DetailView):
     model = Product
 
