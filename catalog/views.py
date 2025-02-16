cb56bf5a18cbfef43cf2a5a9d78576539de9ecad
mihail <mihail.bashkatov@gmail.com>
2025-02-16 13:53:46 +0100

    Adding decorator for cache in ProductListView and set cache functions in catalog/views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.cache import cache_page
+from django.core.cache import cache
 from django.utils.decorators import method_decorator
 from django.urls import reverse_lazy
 from django.views.generic import (
@@ -41,17 +42,21 @@ class HomeTemplateView(TemplateView):
     template_name = "catalog/home.html"
 
 
-# The class for page with all products
+# The class for page with all products. With decorator for cashes
+@method_decorator(cache_page(60 * 15), name='dispatch')
 class ProductsListView(ListView):
     model = Product
 
     def get_queryset(self):
-        """Return only published products."""
-        queryset = super().get_queryset()
-        return queryset.filter(product_is_published=True)
+        """Return only published products. If queryset is not cached, set cache_queryset"""
+        cache_queryset = cache.get('products_queryset')
+        if not cache_queryset:
+            cache_queryset = super().get_queryset()
+            cache.set('products_queryset', cache_queryset, 60 * 15)
+        return cache_queryset.filter(product_is_published=True)
 
 
-# The class for viewing page for particular product. With decorator fo cashes
+# The class for viewing page for particular product. With decorator for caches
 @method_decorator(cache_page(60 * 15), name='dispatch')
 class ProductDetailView(LoginRequiredMixin, DetailView):
     model = Product
